372249bcf657a4e2eddb99ed400685d7eba595fc
ahmedsharaf7 <ahmed6datlst@gmail.com>
2026-09-02 21:51:27 +0300

    Profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import ForgotPassword from "./pages/ForgotPassword";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-
       <Route path="/dashboard" element={ <ProtectedRoute> <Dashboard /> </ProtectedRoute>}/>
       <Route path="/forgot-password" element={<ForgotPassword />} />
-   
+       <Route path="/profile" element={<Profile />} />
+
+
 
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
-      <Route path="/profile" element={<Profile />} />
+     
 
     </Routes>
   );
