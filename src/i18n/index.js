df05442f0036014f2b62048d23ae6bb041a2d698
taha ahmed <tahahodhod650@gmail.com>
2026-09-05 02:20:27 +0300

    intial the i18next library and wrapping this in app.js

---
+++ src/i18n/index.js
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./locales/en.json";
+import ar from "./locales/ar.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    ar: {
+      translation: ar,
+    },
+  },
+
+  lng: "en",
+  fallbackLng: "en",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+i18n.on("languageChanged", (lng) => {
+  document.documentElement.lang = lng;
+  document.documentElement.dir = lng === "ar" ? "rtl" : "ltr";
+});
+
+export default i18n;
