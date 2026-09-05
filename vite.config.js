e7d0ab30ac7e265970f326c6a33ec7cad505c5c7
ahmedsharaf7 <ahmed6datlst@gmail.com>
2026-09-05 22:27:36 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+  plugins: [react(),tailwindcss()],
+   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   server: {
